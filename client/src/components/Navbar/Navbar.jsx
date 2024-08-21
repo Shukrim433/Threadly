4f51fc2dded63dde1108dc0fd93bc30828179b75
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-08-21 16:50:49 +0100

    navbar styling done

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,185 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+import { Link, NavLink } from "react-router-dom";
+import { assets } from "../../assets/assets";
+
+const Navbar = () => {
+  const [visible, setVisible] = useState(false);
+  return (
+    <div className="flex items-center justify-between py-5 text-gray-700 font-medium">
+      <h1 className="w-36">LOGO</h1>
+
+      <ul className="hidden sm:flex gap-5 text-sm text-gray-700 ">
+        <NavLink to="/" className="flex flex-col items-center gap-1">
+          <p>HOME</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+        <NavLink to="/about" className="flex flex-col items-center gap-1">
+          <p>ABOUT</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+        <NavLink to="/contact" className="flex flex-col items-center gap-1">
+          <p>CONTACT</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+      </ul>
+
+      <div className="flex items-center gap-6">
+        {/* search icon */}
+        <div className="w-5 cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+            />
+          </svg>
+        </div>
+        <div className="group relative">
+          {/* profile icon */}
+          <div className="w-5 cursor-pointer">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+              />
+            </svg>
+          </div>
+          {/* hover over profile icon = show div w/ other links */}
+          <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
+              <p className="cursor-pointer hover:text-black">My profile</p>
+              <p className="cursor-pointer hover:text-black">Orders</p>
+              <p className="cursor-pointer hover:text-black">Logout</p>
+            </div>
+          </div>
+        </div>
+        {/* cart icon */}
+        <Link to="/cart" className="relative">
+          <div className="w-5 min-w-5">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+              />
+            </svg>
+          </div>
+          {/* count for no of items in cart */}
+          <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-red-700 text-white aspect-square rounded-full text-[8px]">
+            3
+          </p>
+        </Link>
+        {/* menu icon -  only displayed on small screens */}
+        <div
+          onClick={() => {
+            setVisible(true);
+          }}
+          className="w-5 cursor-pointer sm:hidden"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </div>
+      </div>
+
+      {/* sidebar menu for small screens  - dynamic class names dependant on "visible" state*/}
+      <div
+        className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${
+          visible ? `w-full` : `w-0`
+        }`}
+      >
+        <div className="flex flex-col text-gray-600">
+          {/* back arrow icon -  to close sidebar menu */}
+          <div
+            onClick={() => {
+              setVisible(false);
+            }}
+            className="flex items-center my-4 gap-4 p-3 h-4 cursor-pointer"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+              />
+            </svg>
+          </div>
+          <NavLink
+            to="/"
+            onClick={() => {
+              setVisible(false);
+            }}
+            className="py-2 pl-6 border"
+          >
+            HOME
+          </NavLink>
+          <NavLink
+            to="/about"
+            onClick={() => {
+              setVisible(false);
+            }}
+            className="py-2 pl-6 border"
+          >
+            ABOUT
+          </NavLink>
+          <NavLink
+            to="/contact"
+            onClick={() => {
+              setVisible(false);
+            }}
+            className="py-2 pl-6 border"
+          >
+            CONTACT
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
+
+/* NavLink vs Link : 
+do the same thing except NavLink has extra capabilities such as- u can know
+when a link is in an active/pending state - so u can change css styles according to the link status */
