30179c90cffb6797830ab23e0ed755af766b8fa4
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-08-23 16:26:05 +0100

    created user model

---
+++ server/models/user.model.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    surname: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    /*   orders: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Product",
+    },
+  ], */
+  },
+  { timestamps: true } // for createdAt and updatedAt fields - client - member since <user.createdAt>
+);
+
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
