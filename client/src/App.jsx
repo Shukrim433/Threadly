bff727e2347ec1aa9f76a0dec9e33c854fb402a1
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-08-30 12:05:38 +0100

    added auth protection to some pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { useShopContext } from "./context/ShopContext";
+
 import Navbar from "./components/Navbar/Navbar";
-import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Product from "./pages/Product";
@@ -9,25 +14,26 @@ import Login from "./pages/Login";
 import PlaceOrder from "./pages/PlaceOrder";
 import Orders from "./pages/Orders";
 import Contact from "./pages/Contact";
-import Footer from "./components/Footer/Footer";
 import Signup from "./pages/Signup";
 
 const App = () => {
+  const { authenticatedUser } = useShopContext();
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={authenticatedUser ? <Navigate to="/"/> : <Login />} />
+        <Route path="/signup" element={authenticatedUser ? <Navigate to="/"/> : <Signup />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/product/:productId" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
         <Route path="/place-order" element={<PlaceOrder />} />
         <Route path="/orders" element={<Orders />} />
       </Routes>
-      <Footer/>
+      <Toaster />
+      <Footer />
     </div>
   );
 };
