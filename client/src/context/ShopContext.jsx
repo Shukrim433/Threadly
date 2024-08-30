52105e55d74f0a4b9d4d89261238e56886970586
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-08-30 12:07:46 +0100

    created shop context provider

--- client/src/context/ShopContext.jsx
+++ client/src/context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 // CONTEXT
 export const ShopContext = createContext();
@@ -9,14 +9,17 @@ export const useShopContext = () => {
 };
 
 // PROVIDER
-export const ShopContextProvider = ({children}) => {
-    const value = {
+export const ShopContextProvider = ({ children }) => {
+  // state holds the logged in user
+  const [authenticatedUser, setAuthenticatedUser] = useState(
+    JSON.parse(localStorage.getItem("logged-in-user")) || null
+  );
 
-    }
+  // object holds all the values you want available globally to all components
+  const value = {
+    authenticatedUser: authenticatedUser,
+    setAuthenticatedUser: setAuthenticatedUser,
+  };
 
-    return(
-        <ShopContext.Provider value={value}>
-            {children}
-        </ShopContext.Provider>
-    )
-}
+  return <ShopContext.Provider value={value}>{children}</ShopContext.Provider>;
+};
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import { ShopContextProvider } from "./context/ShopContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
   </BrowserRouter>
 );
