ffc8cd39375c73ec57897d9ca049ad737650d306
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-08-31 14:49:48 +0100

    route and controller to get single product from database

--- server/controllers/product.controllers.js
+++ server/controllers/product.controllers.js
@@ -12,3 +12,19 @@ export const getAllProducts = async (req, res) => {
     res.status(500).json({ error: "internal server error" });
   }
 };
+
+export const getSingleProduct = async (req, res) => {
+  try {
+    const productData = await Product.findOne({
+      _id: req.params.productId,
+    }).populate("category");
+    if (!productData) {
+      res.status(404).json({ error: "no product with that id found" });
+    }
+
+    res.status(200).json(productData);
+  } catch (error) {
+    console.log("error in getSingleProduct controller", error.message);
+    res.status(500).json({ error: "internal server error" });
+  }
+};
--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { getAllProducts} from "../controllers/product.controllers.js";
+import { getAllProducts, getSingleProduct} from "../controllers/product.controllers.js";
 
 const router = express.Router();
 
 // GET /api/products/
 router.get("/", getAllProducts);
+
+// GET /api/products/:productId
+router.get("/:productId", getSingleProduct);
 
 export default router;
