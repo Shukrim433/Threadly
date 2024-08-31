50e3582f5112e22a0d41f6265f79f63040f00068
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-08-31 14:51:33 +0100

    added post list component to home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Hero from "../components/Hero/Hero";
+import ProductList from "../components/ProductList/ProductList";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+
+      <ProductList/>
     </div>
   );
 };
