468e0841ea2c277ea87cdef2bb9f29c295f976e1
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-08-31 14:50:42 +0100

    product component done (no styling)

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useGetSingleProduct from "../hooks/useGetSingleProduct";
+import { useShopContext } from "../context/ShopContext";
 
 const Product = () => {
-  return <div>Product</div>;
+  const { loading, product } = useGetSingleProduct();
+  return (
+    <div>
+      <p>{product.name}</p>
+      <img src={product.image} alt="product photo" />
+    </div>
+  );
 };
 
 export default Product;
