a37a7fb2bdceda083af7414bc2113c010c15a9f8
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-09-07 14:25:28 +0100

    added searchBar and collection page components to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,18 +15,22 @@ import PlaceOrder from "./pages/PlaceOrder";
 import Orders from "./pages/Orders";
 import Contact from "./pages/Contact";
 import Signup from "./pages/Signup";
+import Collection from "./pages/Collection";
+import SearchBar from "./components/SearchBar/SearchBar";
 
 const App = () => {
   const { authenticatedUser } = useShopContext();
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
       <Navbar />
+      <SearchBar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={authenticatedUser ? <Navigate to="/"/> : <Login />} />
         <Route path="/signup" element={authenticatedUser ? <Navigate to="/"/> : <Signup />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/collection" element={<Collection/>} />
         <Route path="/products/:productId" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/place-order" element={<PlaceOrder />} />
