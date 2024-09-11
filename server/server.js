a5458eda786a7ef3e0d56438ce267e260d033ff8
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-09-11 12:53:58 +0100

    deployment

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import dotenv from "dotenv"; // allows you to use values in env file
 dotenv.config(); // put up here so it loads .env variables ASAP.
 import express from "express";
@@ -13,6 +14,8 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); // to parse the incoming request with JSON payloads (from req.body)
 app.use(cookieParser()); // to parse the incoming cookies from req.cookies
 
@@ -20,6 +23,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/order", orderRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 /* app.get("/", (req, res) => {
   // root route http://localhost:5000/
