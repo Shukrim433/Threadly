8d6d58e4cc9745a3ecba2ccdca28a3bcdfd46629
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-09-14 12:37:34 +0100

    made footer stck to bottom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,7 +22,7 @@ import Verify from "./pages/Verify";
 const App = () => {
   const { authenticatedUser } = useShopContext();
   return (
-    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+    <div className="App px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
       <Navbar />
       <SearchBar/>
       <Routes>
